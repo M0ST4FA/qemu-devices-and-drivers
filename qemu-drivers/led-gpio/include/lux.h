386dcb0065c216e03f638e5203945b7e29921e88
M0sT4fa <mostafa2018a3s@gmail.com>
2026-05-23 13:48:51 +0200

    feat(led): finish smart led cdev

--- qemu-drivers/led-gpio/include/lux.h
+++ qemu-drivers/led-gpio/include/lux.h
@@ -1,13 +1,20 @@
 #pragma once
+#include <linux/cdev.h>
 #include <linux/pci.h>
 
 #define LUX_CORE_DRIVER_NAME "lux-core"
 #define LUX_CHIP_LABEL "lux-gpio-chip"
+#define LUX_CHAR_DEVICE_NAME "lux-chardev"
+#define LUX_CLASS_NAME "lux"
+
 #define LUX_VENDOR_ID 0x1234
 #define LUX_DEVICE_ID 0x0001
 
 struct lux_device {
 	struct pci_dev *pdev;
+	struct cdev cdev;
+	dev_t cdev_id;
+	struct device *device;
 	void __iomem *bar[2];
 };
 
---
+++ qemu-drivers/led-gpio/lux-cdev.c
@@ -0,0 +1,156 @@
+#include "asm/io.h"
+#include "linux/cdev.h"
+#include "linux/container_of.h"
+#include "linux/device.h"
+#include "linux/device/class.h"
+#include "linux/err.h"
+#include "linux/fs.h"
+#include "linux/printk.h"
+#include "linux/stddef.h"
+#include "linux/types.h"
+#include <linux/module.h>
+
+#include "../../qemu-devices/led-gpio/include/hw.h"
+#include "linux/uaccess.h"
+#include "lux.h"
+
+struct class *lux_class;
+
+static int lux_smart_open(struct inode *inode, struct file *filp) {
+	struct lux_device *device = NULL;
+
+	device = container_of(inode->i_cdev, struct lux_device, cdev);
+
+	filp->private_data = device;
+
+	return 0;
+}
+
+static int lux_smart_release(struct inode *inode, struct file *filp) {
+
+	return 0;
+}
+
+static ssize_t lux_smart_write(struct file *filp, const char *buf,
+							   size_t count, loff_t *offset) {
+	struct lux_device *device = filp->private_data;
+	struct smart_led led;
+	int led_id;
+
+	if (count != sizeof(struct smart_led))
+		return -EINVAL;
+
+	if (*offset >= LED_NR * sizeof(struct smart_led))
+		return -ERANGE;
+
+	led_id = *offset / sizeof(struct smart_led);
+
+	if (copy_from_user(&led, buf, sizeof(struct smart_led)))
+		return -EFAULT;
+
+	void *led_base = device->bar[1] + led_id * sizeof(struct smart_led);
+
+	writeq(*(uint64_t *)&led, led_base);
+
+	*offset += sizeof(struct smart_led);
+
+	return count;
+}
+
+static ssize_t lux_smart_read(struct file *filp, char *buf,
+							  size_t count, loff_t *offset) {
+	struct lux_device *device = filp->private_data;
+	struct smart_led led;
+	int led_id;
+
+	if (count != sizeof(struct smart_led))
+		return -EINVAL;
+
+	if (*offset >= LED_NR * sizeof(struct smart_led))
+		return -ERANGE;
+
+	led_id = *offset / sizeof(struct smart_led);
+	void *led_base = device->bar[1] + led_id * sizeof(struct smart_led);
+
+	*(uint64_t *)&led = readq(led_base);
+
+	if (copy_to_user(buf, &led, sizeof(struct smart_led)))
+		return -EFAULT;
+
+	*offset += sizeof(struct smart_led);
+
+	return count;
+}
+
+static struct file_operations lux_fops = {
+	.owner = THIS_MODULE,
+	.open = lux_smart_open,
+	.release = lux_smart_release,
+	.write = lux_smart_write,
+	.read = lux_smart_read,
+	.llseek = default_llseek,
+};
+
+static int __init lux_cdev_init(void) {
+	int ret = 0;
+
+	// 1. Register major and minor numbers
+	ret = alloc_chrdev_region(&global_lux->cdev_id, 0, 1, LUX_CHAR_DEVICE_NAME);
+	if (ret < 0) {
+		pr_err(LUX_CHAR_DEVICE_NAME ": Failed to allocate major and minor numbers for lux device");
+		return ret;
+	}
+
+	cdev_init(&global_lux->cdev, &lux_fops);
+	ret = cdev_add(&global_lux->cdev, global_lux->cdev_id, 1);
+	if (ret < 0) {
+		pr_err(LUX_CHAR_DEVICE_NAME ": Failed to add cdev to VFS");
+		goto cleanup;
+	}
+
+	lux_class = class_create(LUX_CLASS_NAME);
+	if (IS_ERR(lux_class)) {
+		pr_err(LUX_CHAR_DEVICE_NAME ": Failed to create class");
+		ret = PTR_ERR(lux_class);
+		goto cleanup;
+	}
+
+	global_lux->device = device_create(lux_class, NULL,
+									   global_lux->cdev_id, global_lux,
+									   LUX_CLASS_NAME "-%d", MINOR(global_lux->cdev_id));
+	if (IS_ERR(global_lux->device)) {
+		pr_err(LUX_CHAR_DEVICE_NAME ": Failed to create sysfs device");
+		ret = PTR_ERR(global_lux->device);
+		goto cleanup;
+	}
+
+	return 0;
+cleanup:
+
+	if (!IS_ERR(global_lux->device))
+		device_destroy(lux_class, global_lux->cdev_id);
+
+	if (!IS_ERR(lux_class))
+		class_destroy(lux_class);
+
+	unregister_chrdev_region(global_lux->cdev_id, 1);
+
+	return 0;
+}
+
+static void __exit lux_cdev_exit(void) {
+	if (global_lux->device)
+		device_destroy(lux_class, global_lux->cdev_id);
+
+	if (lux_class)
+		class_destroy(lux_class);
+
+	unregister_chrdev_region(global_lux->cdev_id, 1);
+	cdev_del(&global_lux->cdev);
+}
+
+module_init(lux_cdev_init);
+module_exit(lux_cdev_exit);
+MODULE_AUTHOR("m0st4fa");
+MODULE_DESCRIPTION("Character driver for exposing smart LED functionality of Lux");
+MODULE_LICENSE("GPL");

aaa16a365c768aaaf33b24d213e839ef36d953be
M0sT4fa <mostafa2018a3s@gmail.com>
2026-05-26 10:49:31 +0200

    feat(led): print-to-led can now set direction

--- user-clients/print-to-led.c
+++ user-clients/print-to-led.c
@@ -2,9 +2,11 @@
 #include <err.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <linux/gpio.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/ioctl.h>
 #include <time.h>
 #include <unistd.h>
 
@@ -14,12 +16,12 @@
 #define LED_DEFAULT_COLOR (0xFF00FFFF)
 
 static inline void print_usage_exit(void) {
-	printf("Usage: print-to-led <string>\n");
+	printf("Usage: print-to-led <gpio_chip_path> <string>\n");
 	exit(EXIT_SUCCESS);
 }
 
 void get_led_from_char(const char c, int y, int x, struct smart_led *led) {
-	uint8_t row = font[c][y];
+	uint8_t row = font[(uint8_t)c][y];
 
 	int32_t led_state = (row >> (7 - x)) & 1;
 
@@ -30,19 +32,49 @@ void get_led_from_char(const char c, int y, int x, struct smart_led *led) {
 	led->color[3] = 255;
 }
 
-int main(int argc, char *argv[]) {
+[[nodiscard("You must handle the error. If this fails, GPIO pins may not be configured for output.")]]
+static int configure_gpio_lines(int gpiochip_fd) {
 	int ret = 0;
+	struct gpio_v2_line_request req = {0};
+	req.num_lines = 64;
+	for (int i = 0; i < 64; i++) {
+		req.offsets[i] = i;
+	}
 
-	if (argc != 2)
+	req.config.attrs[0].attr.id = GPIO_V2_LINE_ATTR_ID_FLAGS;
+	req.config.attrs[0].attr.flags = GPIO_V2_LINE_FLAG_OUTPUT;
+	req.config.attrs[0].mask = 0xFFFFFFFFFFFFFFFFULL;
+	req.config.num_attrs = 1;
+
+	ret = ioctl(gpiochip_fd, GPIO_V2_GET_LINE_IOCTL, &req); // Lock the pins for your process
+
+	if (ret >= 0)
+		ret = req.fd; // You can use it to configure later!
+
+	return ret;
+}
+
+int main(int argc, char *argv[]) {
+	int gpiochip_fd, gpiochip_config_fd, lux_fd;
+
+	if (argc != 3)
 		print_usage_exit();
 
-	char *str = argv[1];
+	const char *gpiochip_path = argv[1];
+	const char *str = argv[2];
 	size_t str_len = strlen(str);
 
 	struct smart_led led;
 
-	int fd = open("/dev/lux-0", O_WRONLY | O_CLOEXEC);
-	if (fd < 0)
+	gpiochip_fd = open(gpiochip_path, O_WRONLY | O_CLOEXEC);
+	if (gpiochip_fd < 0)
+		err(EXIT_FAILURE, "open %s", gpiochip_path);
+	gpiochip_config_fd = configure_gpio_lines(gpiochip_fd);
+	if (gpiochip_config_fd < 0)
+		err(EXIT_FAILURE, "ioctl %s", gpiochip_path);
+
+	lux_fd = open("/dev/lux-0", O_WRONLY | O_CLOEXEC);
+	if (lux_fd < 0)
 		err(EXIT_FAILURE, "open %s", "/dev/lux-0");
 
 	for (size_t i = 0; i < str_len; i++) {
@@ -52,15 +84,19 @@ int main(int argc, char *argv[]) {
 			for (int x = 0; x < COL_NR; x++) {
 				get_led_from_char(c, y, x, &led);
 				int led_id = y * 8 + x;
-				pwrite(fd, &led, sizeof(led), led_id * sizeof(led));
+				pwrite(lux_fd, &led, sizeof(led), led_id * sizeof(led));
 
 				struct timespec ts = {.tv_sec = 0, .tv_nsec = 1000000};
 				nanosleep(&ts, NULL);
 			}
 		}
 
-		ret = sleep(1);
+		sleep(1);
 	}
+
+	close(lux_fd);
+	close(gpiochip_config_fd);
+	close(gpiochip_fd);
 
 	return 0;
 }
